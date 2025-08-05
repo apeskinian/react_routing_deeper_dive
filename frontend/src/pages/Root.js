d1577f1e2511cc51decb58850fffe509ef9c2535
Matthew Aitken <matt@itken.net>
2025-08-05 17:08:29 +0100

    Refactor loader

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 function RouteLayout() {
+    // useNavigation gives us the status of loading.
+    // const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
                 <Outlet />
             </main>
         </>
